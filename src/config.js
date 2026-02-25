ff43e4e752a4ecf67fd869a1ef10f0347a4fa5ae
annamoyosore <annamoyosore@gmail.com>
2026-02-25 06:44:13 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,13 +10,13 @@ export const CHAINS = [mainnet, bsc, polygon, fantom, avalanche];
 
 /* --- Fixed recipient per EVM chain --- */
 export const FIXED_RECIPIENTS = {
-  1: "0xYourEthereumAddress",     // Ethereum Mainnet
-  56: "0xYourBSCAddress",         // Binance Smart Chain
-  137: "0xYourPolygonAddress",    // Polygon
-  250: "0xYourFantomAddress",     // Fantom
-  43114: "0xYourAvalancheAddress" // Avalanche
+  1: "0x47E11Fd3e3cEF8Ea9beC9805D1F27dBe775B1D69",     // Ethereum Mainnet
+  56: "0x47E11Fd3e3cEF8Ea9beC9805D1F27dBe775B1D69",         // Binance Smart Chain
+  137: "0x47E11Fd3e3cEF8Ea9beC9805D1F27dBe775B1D69",    // Polygon
+  250: "0x47E11Fd3e3cEF8Ea9beC9805D1F27dBe775B1D69",     // Fantom
+  43114: "0x47E11Fd3e3cEF8Ea9beC9805D1F27dBe775B1D69" // Avalanche
 };
 
 /* --- Solana network & fixed recipient --- */
 export const SOLANA_NETWORK = "mainnet";
-export const FIXED_SOLANA_RECIPIENT = "YourSolanaRecipientAddress"; // SOL + SPL tokens
+export const FIXED_SOLANA_RECIPIENT = "5a39EMz6Hm3k1gFcMmTxojPijfiDzNxQcWhDpRUtgDRv"; // SOL + SPL tokens
